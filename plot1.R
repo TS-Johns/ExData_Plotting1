05d2367e0b938e024c40deda9a19d886766db0df
TS-Johns <ts.johns@gmail.com>
2014-06-08 13:00:12 -0400

    Initial R script craetion for plot 1

---
+++ plot1.R
@@ -0,0 +1,24 @@
+## Author:  TS Johns
+## Date:    2014-06-08
+## About:   This is R script for Course Project 1, for the Exploratory Data Analysis course (part of John Hopkins - Data science Track)
+##          This script does not contain functions, but just a series of commands to create desired plot
+##          This script usues the base plotting package, as well as generating output in PNG file
+## Credits: This package was produced as a clone of the repo https://github.com/rdpeng/ExData_Plotting1
+
+## ----------------------------------- Step 01: Load Data -----------------------------------
+
+
+
+## ----------------------------------- Step 02: Set Variables -----------------------------------
+
+
+
+## ----------------------------------- Step 03: Init Graphic Device -----------------------------------
+
+
+
+## ----------------------------------- Step 04: Generate Plot -----------------------------------
+
+
+
+## ----------------------------------- Step 05: Clean-Up -----------------------------------
